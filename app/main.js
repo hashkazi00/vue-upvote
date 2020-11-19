0429a9eff6f99f017a9c3be6720862682bde5a56
hashkazi00 <sahilkazi01@hotmail.com>
2020-11-19 15:49:18 +0530

    First Commit

---
+++ app/main.js
@@ -0,0 +1,66 @@
+const cardComponent = {
+  template: `
+  <div style="display:flex; width:100%;">
+    <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">{{submission.title}}</p>
+                </header>
+                <div class="card-content">
+                  <div class="content">{{submission.description}}</div>
+                  <hr />
+                  {{submission.votes}}
+                </div>
+                <footer class="card-footer">
+                  <a
+                    href="#"
+                    class="card-footer-item"
+                    @click="upvote(submission.id)"
+                    >Upvote</a
+                  >
+                  <a
+                    href="#"
+                    class="card-footer-item"
+
+                    <!-- Note that we have used kebab case, mainly due to vue naming  conventions and secondly due to the fact that browser itself cannot differentiate between the fact that "myEvent" and "myevent" are diiferent -->
+                    @click="$emit('delete-entry', submission.id)"
+                    >Delete</a
+                  >
+                </footer>
+              </div>
+  </div>
+  `,
+  props: ['submission', 'submissions'],
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(
+        (submission) => submission.id === submissionId
+      );
+      //an exemplar of reactivity
+      submission.votes++;
+    },
+  },
+};
+
+new Vue({
+  el: '#app',
+  data: {
+    submissions: Seed.submissions,
+  },
+
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => b.votes - a.votes);
+    },
+  },
+
+  methods: {
+    deleteEntry(id) {
+      this.submissions = this.submissions.filter((sub) => {
+        return sub.id !== id;
+      });
+    },
+  },
+  components: {
+    'card-component': cardComponent,
+  },
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+Supposed to be the starting point for first day Vue'ers
+
+All the requirements for the project vue, bulma, fontawesome,etc come from CDNs
